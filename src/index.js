df491c1e2653ab605101fe9087b7ef32afe72cd1
Joanna <astronautnemo@gmail.com>
2022-12-01 15:19:49 +0300

    Completed required deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // Get the form element
+  let form = document.getElementById('create-task-form');
+
+  // On submitting the form, carry out the creating of new todo item
+  form.addEventListener('submit', (event) => {
+      // Prevent the submit button from reloading the page
+    event.preventDefault();
+
+    // Add todo to list
+    addToDo();
+    
+    // Clear the form after submitting
+    form.reset();
+  })
 });
+
+function addToDo(){
+  // Get the input text from the input field and save it in a variable
+  let todo = document.getElementById('new-task-description').value
+  
+  // Create a paragraph element to hold the value
+  let listItem = document.createElement('li');
+  listItem.textContent = todo;
+  
+  // Add the paragraph element to the page
+  document.getElementById('tasks').appendChild(listItem);
+}
