4c9a8b602f3954d84939bad1fb40439b875464a2
Joanna <astronautnemo@gmail.com>
2022-12-01 15:34:31 +0300

    Added deleted button

--- src/index.js
+++ src/index.js
@@ -21,8 +21,24 @@ function addToDo(){
   
   // Create a paragraph element to hold the value
   let listItem = document.createElement('li');
-  listItem.textContent = todo;
+  listItem.textContent = `${todo} `;
+
+  // Create a delete button and add to the list item
+  let delButton = document.createElement('button');
+  // Add event listener to delete listItem on click
+  delButton.addEventListener('click', deleteToDo);
+  // Style the button
+  delButton.textContent = 'X';
+  delButton.width = '50px';
+  delButton.height = '50px';
+  // Append to listItem
+  listItem.appendChild(delButton);
   
   // Add the paragraph element to the page
   document.getElementById('tasks').appendChild(listItem);
+}
+
+function deleteToDo(event){
+  // Delete the parent node aka the listItem
+  event.target.parentNode.remove();
 }
